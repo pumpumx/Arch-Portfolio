6c53158a95de81b0af41629bda89b8f378dadd82
pumpumx <trishuchibhot14@gmail.com>
2025-07-08 20:10:36 +0530

    deployment error fixed

--- src/lazyIndex.ts
+++ src/lazyIndex.ts
@@ -1,4 +1,4 @@
 import { lazy } from "react";
 
-export const lazyMainScreen = lazy(()=>import('/home/pumpum/coding/arch-portfolio/src/components/mainScreen/MainScreen.tsx'))
-export const lazyChrome = lazy(()=>import('/home/pumpum/coding/arch-portfolio/src/apps/chrome/ChromeMain.tsx'))
+export const lazyMainScreen = lazy(()=>import('./components/mainScreen/MainScreen.tsx'))
+export const lazyChrome = lazy(()=>import('./apps/chrome/ChromeMain.tsx')) 
