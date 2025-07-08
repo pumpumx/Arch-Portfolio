583f7ca0d5a50fa7987cd3ad19134289b19a7060
pumpumx <trishuchibhot14@gmail.com>
2025-07-08 20:21:31 +0530

    Icons error fixed

--- src/components/loadingScreen/LoadingScreen.tsx
+++ src/components/loadingScreen/LoadingScreen.tsx
@@ -7,7 +7,7 @@ function LoadingScreen() {
       <div className="w-full h-screen bg-black flex items-center justify-center">
         <div className="w-[50%] h-[80%] flex flex-col items-center justify-center bg-black">
           <div className="w-[80%] h-[50%] flex items-end justify-center">
-            <img src="src/assets/arch.png" alt="" className="bg-contain"/>
+            <img src="/assets/arch.png" alt="" className="bg-contain"/>
           </div>
          <div className="w-[80%] h-[50%]  flex items-center mb-10  justify-center">
             <LoadingAnimation/>
